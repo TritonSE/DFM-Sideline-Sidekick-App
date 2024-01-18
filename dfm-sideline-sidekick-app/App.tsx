dd6fdf63307f3f39f6744943b77f97c4c0187a5d
Megan Leong <meganleong10@gmail.com>
2024-01-18 13:59:13 -0800

    set up gen princ page

--- dfm-sideline-sidekick-app/App.tsx
+++ dfm-sideline-sidekick-app/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import GeneralPrinciples from "./generalPrinciples";
 
 export default function App() {
   return (
     // eslint-disable-next-line @typescript-eslint/no-use-before-define
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <GeneralPrinciples/>
       <StatusBar style="auto" />
     </View>
   );
