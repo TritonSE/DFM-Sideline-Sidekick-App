c854c3e2917330a581b22e2e6f9e6e9d292ad4e6
pratyush1718 <pratyush.chand@outlook.com>
2024-01-24 20:33:08 -0800

    lint check

--- dfm-sideline-sidekick-app/icons/circleIcon.tsx
+++ dfm-sideline-sidekick-app/icons/circleIcon.tsx
@@ -1,7 +1,6 @@
 import { SearchIcon } from '../icons/searchIcon';
-import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Svg, Circle, G } from 'react-native-svg'; // Import G from react-native-svg
+import { Svg, Circle, G } from 'react-native-svg'; 
 
 const styles = StyleSheet.create({
   container: {
@@ -17,10 +16,8 @@ export const CircleIcon = ({fillColor}) => {
   return (
     <View style={styles.container}>
       <Svg width={89.307} height={89.307}  viewBox="0 0 90 91" fill="none">
-        {/* Draw the Circle */}
         <Circle cx={circleCenterX} cy={circleCenterY} r={circleRadius} fill={fillColor} />
 
-        {/* Draw the SearchIcon within a G element */}
         <G transform={`translate(${circleCenterX - 20}, ${circleCenterY - 20})`}>
           <SearchIcon />
         </G>
--- dfm-sideline-sidekick-app/icons/generalPrinciplesIcon.tsx
+++ dfm-sideline-sidekick-app/icons/generalPrinciplesIcon.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { View } from 'react-native';
 import { Svg, Path, G } from 'react-native-svg';
 
