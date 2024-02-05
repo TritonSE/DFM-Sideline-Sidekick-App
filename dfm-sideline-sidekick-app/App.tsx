bf80da44d9a09cc52e18880b7af12688519950b7
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2024-02-05 15:48:07 -0800

    Modify download function to use api url

--- dfm-sideline-sidekick-app/App.tsx
+++ dfm-sideline-sidekick-app/App.tsx
@@ -1,85 +1,62 @@
-import * as FileSystem from 'expo-file-system';
-import { shareAsync } from 'expo-sharing';
+import * as FileSystem from "expo-file-system";
 import { StatusBar } from "expo-status-bar";
 import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-
   // storing api url
   const url = "http://localhost:3001/api/all";
 
   // directory in local storage to store files at
-  const fileDir = "storage/documents/" + 'expo/';
+  const fileDir = FileSystem.documentDirectory + "expo/";
 
   // download resumable object to call functions on
-  const downloadResumable = FileSystem.createDownloadResumable(
-    "https://api.sampleapis.com/wines/reds",
-    fileDir + 'data.json',
-    {}
-  )
+  const downloadResumable = FileSystem.createDownloadResumable(url, fileDir + "data.json", {});
 
   // Check if directory to place files exists already (creates a new directory if not)
   const checkDirectoryExists = async () => {
-
     // checks if directory info
     const dir = await FileSystem.getInfoAsync(fileDir);
 
-    console.log(fileDir);
-    
+    console.log("Directory path:", fileDir);
+    console.log("Directory info:", dir);
 
     // if it doesn't exist
     if (!dir.exists) {
-      console.log('Creating directory');
+      console.log("Creating directory");
 
       // make a new directory to store the files
-      await FileSystem.makeDirectoryAsync(fileDir, { intermediates: true }) // intermediates make it so that it doesn't throw error when no directory
+      await FileSystem.makeDirectoryAsync(fileDir, { intermediates: true }); // intermediates make it so that it doesn't throw error when no directory
     }
-  }
+  };
 
   const downloadJSON = async () => {
-    const filename = 'data.json';
+    const filename = "data.json";
     const localhost = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
 
     try {
+      // Check if the file already exists
+      const fileInfo = await FileSystem.getInfoAsync(fileDir + filename);
 
-      // find directory or creates it if not found
-      await checkDirectoryExists();
+      if (fileInfo.exists) {
+        console.log("File already exists.");
+      } else {
+        // Find directory or create it if not found
+        await checkDirectoryExists();
 
-      // downloads file from api and stores in result
-      const result = await downloadResumable.downloadAsync();
-      console.log(result);
-
-      // save result to storage
-      // await save(result.uri, filename, result.headers["Content-Type"]);
-
+        // Downloads file from API and stores it in result
+        const result = await downloadResumable.downloadAsync();
+        console.log("Result:", result);
+      }
     } catch (err) {
-      console.log(err);
+      console.log("ERROR:", err);
     }
-  }
-
-  // const save = async (uri, filename, mimetype) => {
-  //   // android needs to ask for storage permission
-  //   if (Platform.OS === 'android') {
-  //     const permission = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-      
-  //     if (permission.granted) {
-  //       const base64 = await FileSystem.readAsStringAsync(uri, {encoding: FileSystem.EncodingType.base64});
-  //       await FileSystem.StorageAccessFramework.createFileAsync(permission.directoryUri, filename, mimetype)
-  //         .then(async (uri) => {
-  //           await FileSystem.writeAsStringAsync(uri, base64, {encoding: FileSystem.EncodingType.base64});
-  //         })
-  //         .catch(e => console.log(e));
-  //     } else {
-  //       shareAsync(uri);
-  //     }
-  //   }
-  // }
+  };
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-use-before-define
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Download here!" onPress={downloadJSON}/>
+      <Button title="Download here!" onPress={downloadJSON} />
       <StatusBar style="auto" />
     </View>
   );
