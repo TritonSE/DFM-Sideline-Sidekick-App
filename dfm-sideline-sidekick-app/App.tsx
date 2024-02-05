f822e104b903cf6a5909fbee6a21d3eb4e7052fc
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-02-05 15:56:49 -0800

    Add android device compatibility and file exist function

--- dfm-sideline-sidekick-app/App.tsx
+++ dfm-sideline-sidekick-app/App.tsx
@@ -1,79 +1,84 @@
 import * as FileSystem from 'expo-file-system';
-import { shareAsync } from 'expo-sharing';
 import { StatusBar } from "expo-status-bar";
 import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
 
+  // "https://api.sampleapis.com/wines/reds"
   // storing api url
-  const url = "http://localhost:3001/api/all";
+  const localhost = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
+  const url = `http://${localhost}:3001/api/all`;
 
   // directory in local storage to store files at
-  const fileDir = "storage/documents/" + 'expo/';
+  const fileDir = FileSystem.documentDirectory + "expo/";
+  const fileName = 'emergencies.json';
 
   // download resumable object to call functions on
   const downloadResumable = FileSystem.createDownloadResumable(
-    "https://api.sampleapis.com/wines/reds",
-    fileDir + 'data.json',
+    url,
+    fileDir + fileName,
     {}
   )
 
+  const checkFileExists = async () => {
+    const filePath = await FileSystem.getInfoAsync(fileDir + fileName);
+
+    // checks if file exists
+    return filePath.exists;
+  }
+
   // Check if directory to place files exists already (creates a new directory if not)
   const checkDirectoryExists = async () => {
-
     // checks if directory info
     const dir = await FileSystem.getInfoAsync(fileDir);
 
-    console.log(fileDir);
-    
+    console.log("Directory path:", fileDir);
+    console.log("Directory info:", dir);
 
     // if it doesn't exist
     if (!dir.exists) {
-      console.log('Creating directory');
+      console.log("Creating directory");
 
       // make a new directory to store the files
-      await FileSystem.makeDirectoryAsync(fileDir, { intermediates: true }) // intermediates make it so that it doesn't throw error when no directory
+      await FileSystem.makeDirectoryAsync(fileDir, { intermediates: true }); // intermediates make it so that it doesn't throw error when no directory
     }
-  }
+  };
 
   const downloadJSON = async () => {
-    const filename = 'data.json';
-    const localhost = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
-
     try {
 
-      // find directory or creates it if not found
-      await checkDirectoryExists();
+      // check if file exists already
+      const fileExists = await checkFileExists();
 
-      // downloads file from api and stores in result
-      const result = await downloadResumable.downloadAsync();
-      console.log(result);
+      let uri;
 
-      // save result to storage
-      // await save(result.uri, filename, result.headers["Content-Type"]);
+      if (!fileExists) {
+
+        // find directory or creates it if not found
+        await checkDirectoryExists();
+
+        // downloads file from api and stores in result
+        const result = await downloadResumable.downloadAsync();
+        uri = result.uri;
+
+      } else {
+        console.log("File already exists");
+
+        uri = fileDir + fileName;
+      }
+
+      const output = await FileSystem.getInfoAsync(uri);
+      const str = await FileSystem.readAsStringAsync(uri);
+
+      const jsonOutput = JSON.parse(str);
+
+      console.log(output);
+      console.log(jsonOutput);
 
     } catch (err) {
       console.log(err);
     }
   }
-
-  // const save = async (uri, filename, mimetype) => {
-  //   // android needs to ask for storage permission
-  //   if (Platform.OS === 'android') {
-  //     const permission = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-      
-  //     if (permission.granted) {
-  //       const base64 = await FileSystem.readAsStringAsync(uri, {encoding: FileSystem.EncodingType.base64});
-  //       await FileSystem.StorageAccessFramework.createFileAsync(permission.directoryUri, filename, mimetype)
-  //         .then(async (uri) => {
-  //           await FileSystem.writeAsStringAsync(uri, base64, {encoding: FileSystem.EncodingType.base64});
-  //         })
-  //         .catch(e => console.log(e));
-  //     } else {
-  //       shareAsync(uri);
-  //     }
-  //   }
-  // }
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-use-before-define
