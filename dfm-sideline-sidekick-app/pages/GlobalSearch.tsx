9d200c0eef654a8fd90d8564bb5746ab95d2ef34
Megan Leong <meganleong10@gmail.com>
2024-02-16 02:35:00 -0800

    added x and cancel buttons. allowed filtering to match for documents that partially match and tier the documents highest to lowest from most to least similar to the search query. highlight in blue parts of titles that match search query

--- dfm-sideline-sidekick-app/pages/GlobalSearch.tsx
+++ dfm-sideline-sidekick-app/pages/GlobalSearch.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import { FlatList, Text, TextInput, View } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome"; 
+import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+
+import { searchDocuments } from "../HandleSearch";
 
 import styles from "./GlobalSearchStyles";
 
@@ -11,9 +13,21 @@ type Document = {
 };
 
 const documents: Document[] = [
-  { id: "1", title: "Cervical Spine Injury", subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do." },
-  { id: "2", title: "Cervical Strain", subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing." },
-  { id: "3", title: "Stroke", subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor." },
+  {
+    id: "1",
+    title: "Cervical Spine Injury",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+  },
+  {
+    id: "2",
+    title: "Cervical Strain",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
+  },
+  {
+    id: "3",
+    title: "Stroke",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
 ];
 
 const SearchBarComponent = () => {
@@ -22,43 +36,78 @@ const SearchBarComponent = () => {
 
   const handleSearch = (text: string) => {
     setQuery(text);
-    if (!text.trim()) {
-      setFilteredDocuments([]);
-    } else {
-      const matchedDocuments = documents.filter((doc) =>
-        doc.title.toLowerCase().includes(text.toLowerCase()),
+    const matchedDocuments = searchDocuments(documents, text);
+    setFilteredDocuments(matchedDocuments);
+  };
+
+  const clearInput = () => {
+    setQuery("");
+    setFilteredDocuments([]);
+  };
+
+  const highlightText = (text: string, input: string): React.ReactNode[] => {
+    // Escape special characters in the query for use in a regular expression
+    const escapedQuery = input.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
+    // Build a regex to match the query as whole words
+    const queryRegex = new RegExp(`(\\b${escapedQuery})`, "gi");
+
+    // Split the title by the regular expression to get an array of parts
+    const parts = text.split(queryRegex);
+
+    return parts.map((part, index) => {
+      // Check if the part of the title matches the query and is not just a whitespace
+      const isMatch = queryRegex.test(part) && part.trim() !== "";
+      return (
+        <Text key={index} style={isMatch ? styles.highlightedText : undefined}>
+          {part}
+        </Text>
       );
-      setFilteredDocuments(matchedDocuments);
-    }
+    });
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Global Search</Text>
-      <View style={styles.searchSection}>
-        <Icon name="search" size={15} color="gray" style={styles.searchIcon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Search"
-          value={query}
-          onChangeText={handleSearch}
-          selectionColor="#909090"
-        />
+      <View style={styles.searchContainer}>
+        <View style={styles.searchSection}>
+          <Icon name="search" size={13} color="gray" style={styles.searchIcon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Search"
+            value={query}
+            onChangeText={handleSearch}
+            selectionColor="#909090"
+          />
+          {query.length > 0 && (
+            <TouchableOpacity onPress={clearInput} style={{ padding: 10 }}>
+              <Icon name="x" size={15} color="gray" />
+            </TouchableOpacity>
+          )}
+        </View>
+        <View>
+          {query.length > 0 && (
+            <TouchableOpacity onPress={clearInput} style={styles.cancelButton}>
+              <Text>Cancel</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </View>
-      <FlatList
-        data={filteredDocuments}
-        keyExtractor={(item) => item.id}
-        ItemSeparatorComponent={() => <View style={styles.divider} />}
-        renderItem={({ item }) => (
-          <View style={styles.listItemContainer}>
-            <View style={styles.listItemTextContainer}>
-              <Text style={styles.listItemTitle}>{item.title}</Text>
-              <Text style={styles.listItemSubtitle}>{item.subtitle}</Text>
+      {filteredDocuments.length > 0 && (
+        <FlatList
+          data={filteredDocuments}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <View style={styles.divider} />}
+          renderItem={({ item }) => (
+            <View style={styles.listItemContainer}>
+              <View style={styles.listItemTextContainer}>
+                <Text style={styles.listItemTitle}>{highlightText(item.title, query)}</Text>
+                <Text style={styles.listItemSubtitle}>{item.subtitle}</Text>
+              </View>
+              <Icon name="chevron-right" size={20} color="#909090" />
             </View>
-            <Icon name="chevron-right" size={12} color="#909090" />
-          </View>
-        )}
-      />
+          )}
+        />
+      )}
     </View>
   );
 };
