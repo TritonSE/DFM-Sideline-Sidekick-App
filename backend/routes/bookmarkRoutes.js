a40643625d94106faf302bb818fd5a1ed70ec49a
pratyush1718 <pratyush.chand@outlook.com>
2024-03-02 11:02:42 -0800

    linting

--- backend/routes/bookmarkRoutes.js
+++ backend/routes/bookmarkRoutes.js
@@ -1,28 +1,27 @@
-import { LocalStorage } from 'node-localstorage';
-const localStorage = new LocalStorage('./bookmarks');
+import { LocalStorage } from "node-localstorage";
+const localStorage = new LocalStorage("./bookmarks");
 
 function createBookmark(name) {
-    let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
-    bookmarks.push({ "PageName": name });
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+  bookmarks.push({ PageName: name });
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 }
 
 function deleteBookmark(name) {
-    let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
-    const index = bookmarks.findIndex(bookmark => bookmark.PageName === name);
+  let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+  const index = bookmarks.findIndex((bookmark) => bookmark.PageName === name);
 
-    if (index !== -1) {
-        bookmarks.splice(index, 1);
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    }else{
-        console.log("Bookmark not found!")
-    }
+  if (index !== -1) {
+    bookmarks.splice(index, 1);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  } else {
+    console.log("Bookmark not found!");
+  }
 }
 
 function getAllBookmarks() {
-    return JSON.parse(localStorage.getItem('bookmarks')) || [];
+  return JSON.parse(localStorage.getItem("bookmarks")) || [];
 }
-
 
 /* uncomment for testing purposes!! 
 createBookmark('Example Page');
@@ -30,6 +29,3 @@ deleteBookmark('Example Page');
 const bookmarks = getAllBookmarks();
 console.log(bookmarks);
 */
-
-
-
