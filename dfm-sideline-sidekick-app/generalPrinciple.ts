4d3867626673f4276b2ebd0a7e487a6fc2e053ce
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-04-11 14:59:46 -0700

    Adds generalPrinciple.ts with getter to retrieve general principle

---
+++ dfm-sideline-sidekick-app/generalPrinciple.ts
@@ -0,0 +1,95 @@
+import { get, handleAPIError, post, put } from "./requests";
+
+import type { APIResult } from "./requests";
+
+// export type GeneralPrinciple = {
+//   _id: string;
+//   title: string;
+//   overview?: {
+//     Importance?: string;
+//     "Mechanism of Injury"?: string[];
+//     Diagnosis?: string[];
+//     "Physical Exam"?: string[];
+//   };
+//   content?: object;
+// };
+
+export type GeneralPrinciple = {
+  _id: string;
+  title: string;
+  overview?: object;
+  content?: object;
+};
+
+/**
+ * The expected inputs when we want to create a new Task object. In the MVP, we only
+ * need to provide the title and optionally the description, but in the course of
+ * this tutorial you'll likely want to add more fields here.
+ */
+export type CreateGeneralPrincipleRequest = {
+  title: string;
+  overview?: object;
+  content?: object;
+};
+
+/**
+ * The expected inputs when we want to update an existing Task object. Similar to
+ * `CreateTaskRequest`.
+ */
+export type UpdateGeneralPrincipleRequest = {
+  _id: string;
+  title: string;
+  overview?: object;
+  content?: object;
+};
+
+/**
+ * The implementations of these API client functions are provided as part of the
+ * MVP. You can use them as a guide for writing the other client functions.
+ */
+export async function createGeneralPrinciple(
+  generalPrinciple: CreateGeneralPrincipleRequest,
+): Promise<APIResult<GeneralPrinciple>> {
+  try {
+    const response = await post("/api/generalPrinciple", generalPrinciple);
+    const json = (await response.json()) as GeneralPrinciple;
+    return { success: true, data: json };
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
+
+export async function getGeneralPrinciple(id: string): Promise<APIResult<GeneralPrinciple>> {
+  try {
+    const response = await get(`/api/generalPrinciple/${id}`);
+    const json = (await response.json()) as GeneralPrinciple;
+    return { success: true, data: json };
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
+
+export async function getAllGeneralPrinciples(): Promise<APIResult<GeneralPrinciple[]>> {
+  try {
+    const response = await get(`/api/generalPrinciple/`);
+    const json = (await response.json()) as GeneralPrinciple[];
+    // const parsedJson = json.map((element) => (element));
+    return { success: true, data: json };
+    // your code here
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
+
+export async function updateGeneralPrinciple(
+  generalPrinciple: UpdateGeneralPrincipleRequest,
+): Promise<APIResult<GeneralPrinciple>> {
+  try {
+    // your code here
+    const response = await put(`/api/generalPrinciple/${generalPrinciple._id}`, generalPrinciple);
+    const json = (await response.json()) as GeneralPrinciple;
+    return { success: true, data: json };
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
