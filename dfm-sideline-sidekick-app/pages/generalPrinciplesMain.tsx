c0c51477be3c1358c90cb84e72869a9c6d4c4fb8
Eshaan Sharma <eshaansharma18@gmail.com>
2024-04-11 15:09:41 -0700

    Added generalPrinciplesMain fetch methods

--- dfm-sideline-sidekick-app/pages/generalPrinciplesMain.tsx
+++ dfm-sideline-sidekick-app/pages/generalPrinciplesMain.tsx
@@ -1,12 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
+
 import * as Font from "expo-font";
 import React, { useState } from "react";
 import { Pressable, ScrollView, Text, View } from "react-native";
 
+import { getGeneralPrinciple } from "../generalPrinciple";
+
 
 import SearchPage from "./SearchPage";
 import styles from "./generalPrinciplesMainStyles";
-
 const GeneralPrinciplesMain = () => {
+  const navigation = useNavigation()
+  const [generalPrinciple, setGeneralPrinciple] = useState(null);
+
   const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
   const [searchShowing, setSearchShowing] = useState(false);
 
@@ -25,8 +31,41 @@ const GeneralPrinciplesMain = () => {
       }
     }
 
+
     void loadFont();
   }, []);
+
+  useEffect(() => {
+    async function fetchGeneralPrinciple() {
+      try {
+        //It seems to work on Android and iPad
+        //Test Case: Cervical Spine Injury - demonstrate recursive rendering
+        const generalPrincipleObjectId = "65c586d1f922fa885e8208bd";
+        //Separate Test Case here: To Be Deleted Emergency - demonstrates textual rendering
+        //const emergencyObjectId = "65b369a8e8fe96a404d4fd6b";
+        //Test Case: New Emergency Placeholder Four - demonstrates blank rendering (only title in db)
+        //const emergencyObjectId = "65c2ef26b87b638ac61beb09";
+        //Test Case: Cervical Strain - demonstrates simple placeholder headers
+        //const emergencyObjectId = "65b36f12640d62464e0dd129";
+        const result = await getGeneralPrinciple(generalPrincipleObjectId);
+        if (result.success) {
+          setGeneralPrinciple(result.data);
+        } else {
+          console.error("Error fetching general principle data:", result.error);
+        }
+      } catch (error) {
+        console.error("Error fetching general principle data:", error);
+      }
+    }
+
+    void fetchGeneralPrinciple();
+  }, []);
+
+  const handlePress = () => {
+    if (generalPrinciple !== null) {
+      navigation.navigate("GeneralPrinciples", { generalPrinciple })
+    }
+  }
 
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
@@ -50,7 +89,7 @@ const GeneralPrinciplesMain = () => {
     return pressableData.map((row, index) => (
       <View key={index} style={styles.row}>
         {row.map((label, colIndex) => (
-          <Pressable key={`${index}-${colIndex}`} style={styles.pressable} onPress={() => { console.log(convertToScreenName(label)); }}>
+          <Pressable key={`${index}-${colIndex}`} style={styles.pressable} onPress={handlePress}>
             <Text style={styles.pressableText} numberOfLines={0}>
               {label}
             </Text>
