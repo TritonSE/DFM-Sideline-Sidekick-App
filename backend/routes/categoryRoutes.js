4144f80eca394ee2c353c0bdcf70c148095356a3
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2024-04-19 15:26:17 -0700

    Add validator for category creation

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,23 +1,24 @@
 import express from "express";
 
 import {
-    getAllCategories,
-    getCategoryById,
-    createCategory,
-    deleteCategoryById,
-    addTitleToCategoryById,
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  deleteCategoryById,
+  addTitleToCategoryById,
 } from "../controllers/categoryController.js";
+import { handleValidationErrors, validateCategory } from "../validators/categoryValidator.js";
 
 const router = express.Router();
 
 // GET all categories
-router.get("/categories", getAllCategories)
+router.get("/categories", getAllCategories);
 
 // GET specific category by ID
-router.get("/categories/:id", getCategoryById)
+router.get("/categories/:id", getCategoryById);
 
 // POST category
-router.post("/categories", createCategory);
+router.post("/categories", validateCategory, handleValidationErrors, createCategory);
 
 // DELETE specific category by ID
 router.delete("/categories/:id", deleteCategoryById);
