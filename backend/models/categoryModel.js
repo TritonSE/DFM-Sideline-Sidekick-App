7a3b6f6227f76d9c7726da36652a30bd06ddf352
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2024-04-19 15:35:21 -0700

    Run Prettier to resolve linter error

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-    title: String,
-    items: [],
-    type: {
-        type: String,
-        enum: ["Emergency", "General Principle"],
-        message: '{VALUE} is not supported'
-    },
+  title: String,
+  items: [],
+  type: {
+    type: String,
+    enum: ["Emergency", "General Principle"],
+    message: "{VALUE} is not supported",
+  },
 });
 
 const Category = mongoose.model("Category", categorySchema);
