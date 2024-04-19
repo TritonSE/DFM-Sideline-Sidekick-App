927ee8bb07d959386cad0ff1584b0af2c42b9d89
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2024-04-19 14:48:21 -0700

    Create delete category and add item routes

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
 
-import { createCategory } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  addTitleToCategoryById,
+  deleteCategoryById,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
-router.post(
-    "/categories",
-    createCategory,
-);
+router.post("/categories", createCategory);
+
+router.delete("/categories/:id", deleteCategoryById);
+
+router.put("/categories/:id", addTitleToCategoryById);
 
 export default router;
