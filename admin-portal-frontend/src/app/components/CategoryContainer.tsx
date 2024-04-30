bfdf0a1491232ac06fcca6e22edece682c921e43
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2024-04-30 01:05:07 -0700

    Initial styling for Principles view

--- admin-portal-frontend/src/app/components/CategoryContainer.tsx
+++ admin-portal-frontend/src/app/components/CategoryContainer.tsx
@@ -1,4 +1,6 @@
 import { Category } from "./categoryRoutes";
+import TrashIcon from "../icons/trash.png";
+import EditIcon from "../icons/edit.png";
 
 type CategoryItemProps = {
   id: string;
@@ -9,31 +11,53 @@ type CategoryItemProps = {
 
 const CategoryItem: React.FC<CategoryItemProps> = ({ id, title, pages }) => {
   return (
-    <tr key={id}>
-      <td>{title}</td>
-      <td>{pages}</td>
+    <tr key={id} className="border-b">
+      <td className="w-1/4 text-center py-3">{title}</td>
+      <td className="w-1/4 text-center py-3">
+        {/* Added just to check styling */}
+        <select className="p-1 bg-blue-100 rounded-md">
+          <option value="public">Public</option>
+          <option value="hidden">Hidden</option>
+        </select>
+      </td>
+      <td className="w-1/4 text-center py-3">{pages}</td>
+      <td className="w-1/4 text-center py-3">
+        <button className="mr-3 bg-blue-100 p-2 rounded-full border border-black">
+          <img src={EditIcon.src} alt="Edit" className="w-4 h-4" />
+        </button>
+        <button className="bg-blue-100 p-2 rounded-full border border-black">
+          <img src={TrashIcon.src} alt="Delete" className="w-4 h-4" />
+        </button>
+      </td>
     </tr>
   );
 };
 
-export const CategoryContainer: React.FC<{ items: Category[]; type: string }> = ({ items: categories, type }) => {
+export const CategoryContainer: React.FC<{ items: Category[]; type: string }> = ({
+  items: categories,
+  type,
+}) => {
   return (
-    <table>
+    <table className="w-full">
       {/* table heading */}
       <tbody>
-        <tr>
-          <th>Category Name</th>
-          <th>Visbility</th>
-          <th>Pages</th>
-          <th>Actions</th>
+        <tr className="border-b">
+          <th className="w-1/4 text-center py-3">Category Name</th>
+          <th className="w-1/4 text-center py-3">Visbility</th>
+          <th className="w-1/4 text-center py-3">Pages</th>
+          <th className="w-1/4 text-center py-3">Actions</th>
         </tr>
-        
+
         {categories
           // gets only either emergency or general principle
           .filter((category) => category.type === type)
           .map((category: Category) => {
             return (
-              <CategoryItem id={category._id} title={category.title} pages={category.items.length} />
+              <CategoryItem
+                id={category._id}
+                title={category.title}
+                pages={category.items.length}
+              />
             );
           })}
       </tbody>
