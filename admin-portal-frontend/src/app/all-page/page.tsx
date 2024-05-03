fbc284ded33c80f90c9536c4b0ee725928943724
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-05-02 23:20:05 -0700

    Fix coloring style

--- admin-portal-frontend/src/app/all-page/page.tsx
+++ admin-portal-frontend/src/app/all-page/page.tsx
@@ -18,28 +18,14 @@ export default function CategoriesPage() {
     fetchData();
   }, []);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-full w-full p-20 bg-[#E5EFF5]">
-      <div className="flex flex-row justify-between w-5/6 mb-6">
-        <h1 className="text-start text-2xl font-bold">General Principles</h1>
-        <div>
-          <select className="px-3 py-1 bg-white">
-            <option disabled selected hidden>
-              Filter by
-            </option>
-            <option value="Alphabetical Order">Alphabetical Order</option>
-            <option value="Date added (Earliest)">Date added (Earliest)</option>
-            <option value="Date added (Latest)">Date added (Latest)</option>
-            <option value="Visibility">Visibility</option>
-          </select>
-        </div>
-      </div>
-      <div className="flex flex-col h-5/6 w-5/6 p-10 rounded-md bg-white">
+    return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen p-20 bg-blue-100">
+      <div className="flex flex-col h-screen w-5/6 p-10 rounded-md bg-white">
         <div className="flex flex-row items-center justify-between mb-10">
-          <h2 className="text-2xl">All Categories</h2>
+          <h2 className="text-2xl">All Pages</h2>
           <div className="ml-24">
-            <button className="px-4 py-2 rounded-md text-white bg-[#00629B] mr-2">Edit Order</button>
-            <button className="px-4 py-2 rounded-md text-white bg-[#00629B]">+ Add Category</button>
+            <button className="px-4 py-2 rounded-md text-white bg-blue-600 mr-2">Edit Order</button>
+            <button className="px-4 py-2 rounded-md text-white bg-blue-600">+ Add Page</button>
           </div>
         </div>
         <PageContainer items={categories}></PageContainer>
