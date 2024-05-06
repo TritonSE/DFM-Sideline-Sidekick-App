b9c88eb0412b7fde767fd6626daef7ad3202512b
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2024-05-06 00:15:30 -0700

    Update responsiveness of delete popup

--- admin-portal-frontend/src/app/components/DeletePopup.tsx
+++ admin-portal-frontend/src/app/components/DeletePopup.tsx
@@ -13,12 +13,12 @@ const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
 }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-      <div className="bg-white w-2/5 h-2/6 p-8 rounded-md shadow-md flex flex-col justify-center">
+      <div className="bg-white w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 p-8 rounded-md shadow-md flex flex-col justify-center">
         <button className="flex justify-end mb-4" onClick={onCancel}>
           <img src={CloseIcon.src} alt="Close" className="w-4 h-4" />
         </button>
         <div className="text-center">
-          <h1 className="text-2xl text-[#182B49] font-bold">
+          <h1 className="text-xl sm:text-2xl text-[#182B49] font-bold">
             Are you sure you want to delete
             <br />
             this category?
@@ -26,12 +26,15 @@ const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
           <p className="my-4 text-[#182B49]">
             This category cannot be restored and will not be saved.
           </p>
-          <div className="flex justify-evenly mt-12">
-            <button className="px-4 py-2 bg-white text-[#00629B] rounded-md" onClick={onCancel}>
+          <div className="flex justify-evenly mt-4 sm:mt-6">
+            <button
+              className="px-3 sm:px-4 py-2 bg-white text-[#00629B] rounded-md"
+              onClick={onCancel}
+            >
               No, keep
             </button>
             <button
-              className="mr-2 px-6 py-3 bg-[#00629B] text-white font-bold rounded-md"
+              className="px-4 sm:px-6 py-3 bg-[#00629B] text-white font-bold rounded-md"
               onClick={onDelete}
             >
               Yes, delete
