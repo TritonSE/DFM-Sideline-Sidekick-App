091d7b6be030be0151d86e8761d4a5a816c4ce79
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-05-06 16:47:00 -0700

    Center Test text

--- admin-portal-frontend/src/app/page.tsx
+++ admin-portal-frontend/src/app/page.tsx
@@ -1,16 +1,14 @@
-import HorizontalNavBar from './components/HorizontalNavbar'
-import React from 'react';
-import VerticalNavBar from './components/VerticalNavBar';
-import styles from './pageStyles'
-import EmergencyFlow from './pages/EmergencyFlow';
-
+import HorizontalNavBar from "./components/HorizontalNavbar";
+import React from "react";
+import VerticalNavBar from "./components/VerticalNavBar";
+import styles from "./pageStyles";
+import EmergencyFlow from "./pages/EmergencyFlow";
 
 const AnotherPage: React.FC = () => {
-
   return (
     <div>
       <div style={styles.verticalNavBar}>
-        <VerticalNavBar/>
+        <VerticalNavBar />
       </div>
       <div style={styles.horizontalNavBar}>
         <HorizontalNavBar />
@@ -18,7 +16,6 @@ const AnotherPage: React.FC = () => {
       <div>
         <EmergencyFlow />
       </div>
-      
     </div>
   );
 };
--- admin-portal-frontend/src/app/pages/EmergencyFlow.tsx
+++ admin-portal-frontend/src/app/pages/EmergencyFlow.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import styles from './EmergencyFlowStyles';
-import searchIcon from '../icons/ic_search_grey.png'
-import Image from 'next/image';
+import React from "react";
+import styles from "./EmergencyFlowStyles";
+import searchIcon from "../icons/ic_search_grey.png";
+import Image from "next/image";
 
 const EmergencyFlow: React.FC = () => {
   return (
-    <div>
-        <h1>
-            Test1
-        </h1>
+    <div
+      style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}
+    >
+      <h1>Test1</h1>
     </div>
   );
 };
