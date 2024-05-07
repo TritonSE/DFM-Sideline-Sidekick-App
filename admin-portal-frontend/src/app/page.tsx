bb7ca49ab3338754381199efdb4ce36d7121fc7f
Megan Leong <meganleong10@gmail.com>
2024-05-06 23:14:48 -0700

    added persisting login logic to remember user

--- admin-portal-frontend/src/app/page.tsx
+++ admin-portal-frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import Image from 'next/image';
 import { auth } from './firebase-config';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { setPersistence, signInWithEmailAndPassword, onAuthStateChanged, browserLocalPersistence } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import search from './symbols/search.png';
 
@@ -18,8 +18,8 @@ export default function Home() {
   const [error, setError] = useState<string>('');
   const router = useRouter();
 
-  const buttonStyle = loginForm.email && loginForm.password 
-    ? { backgroundColor: '#00629B' } 
+  const buttonStyle = loginForm.email && loginForm.password
+    ? { backgroundColor: '#00629B' }
     : { backgroundColor: '#6C6C6C', cursor: 'not-allowed' };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -32,27 +32,37 @@ export default function Home() {
     const { email, password } = loginForm;
 
     try {
+      //set auth persistence to local, allowing sessions to persist across browser tabs
+      await setPersistence(auth, browserLocalPersistence);
       await signInWithEmailAndPassword(auth, email, password);
-      router.push('/home'); 
+      router.push('/home');
     } catch (err: any) {
-      setError("The email or password is incorrect."); 
+      setError('The email or password is incorrect.');
     }
   };
 
   useEffect(() => {
+    // listen to auth state changes and automatically navigate if authenticated
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.push('/home');
+      }
+    });
+
     document.body.style.overflow = 'hidden';
     const formFilled = loginForm.email.trim() !== '' && loginForm.password.trim() !== '';
     setIsFormFilled(formFilled);
-    
+
     return () => {
       document.body.style.overflow = 'visible';
+      unsubscribe(); // detach listener to prevent memory leaks
     };
-  }, [loginForm.email, loginForm.password]);
+  }, [loginForm.email, loginForm.password, router]);
 
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md bg-white shadow-xl rounded-lg p-8 flex flex-col items-center mx-4">
-        <Image src={search} alt="Search Icon" width={60} height={60}/> 
+        <Image src={search} alt="Search Icon" width={60} height={60} />
         <h2 className="text-blue-950 text-lg sm:text-xl font-bold my-4">Sideline Sidekick Admin Mode</h2>
         <form onSubmit={handleSubmit} className="w-full">
           <div className="mb-2">
@@ -79,17 +89,15 @@ export default function Home() {
           </div>
           <div className="mb-6 flex items-center">
             <input id="remember-me" type="checkbox" className="mr-2 h-4 w-4 accent-blue border-2 border-blue-900" name="rememberMe" />
-            <label htmlFor="remember-me" className="text-sm sm:text-base">
-              Remember me
-            </label>
+            <label htmlFor="remember-me" className="text-sm sm:text-base">Remember me</label>
           </div>
           <div className="mb-4">
-            <button 
-              type="submit" 
+            <button
+              type="submit"
               style={buttonStyle}
               className={`p-2 w-full text-white font-bold rounded ${loginForm.email && loginForm.password ? 'hover:bg-blue-700' : ''}`}
               disabled={!loginForm.email || !loginForm.password}>
-                Log in
+              Log in
             </button>
             {error && <p className="text-red-600 text-sm">{error}</p>}
           </div>
