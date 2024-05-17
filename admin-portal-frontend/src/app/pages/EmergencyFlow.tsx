fb00c7a62f2aac61998e801a823cf16a2e667ee1
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-05-16 17:54:11 -0700

    Adds functionality to EmergencyFlowPage, commit for deployment testing

--- admin-portal-frontend/src/app/pages/EmergencyFlow.tsx
+++ admin-portal-frontend/src/app/pages/EmergencyFlow.tsx
@@ -1,123 +1,134 @@
+"use client";
+
 import React from "react";
 import styles from "./EmergencyFlowStyles";
 import searchIcon from "../icons/ic_search_grey.png";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { createEmergency, CreateEmergencyRequest } from "../../../emergencies";
+
+export interface EmergencyFlowProps {
+  onSubmit?: () => void;
+}
+
+const InputBlock = ({ label, value, onChange }) => (
+  <>
+    <p style={styles.information}>{label}</p>
+    <input
+      type="text"
+      placeholder="Add details"
+      style={styles.textbox}
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
+    />
+  </>
+);
 
 const EmergencyFlow: React.FC = () => {
+  const [emergencyTitle, setEmergencyTitle] = React.useState("");
+  const [importance, setImportance] = React.useState("");
+  const [riskFactors, setRiskFactors] = React.useState("");
+  const [mechanismOfInjury, setMechanismOfInjury] = React.useState("");
+  const [diagnosis, setDiagnosis] = React.useState("");
+  const [physicalExam, setPhysicalExam] = React.useState("");
+  const [acuteManagement, setAcuteManagement] = React.useState("");
+  const [dispo, setDispo] = React.useState("");
+  const [considerations, setConsiderations] = React.useState("");
+  const router = useRouter();
+
+  const handlePublish = () => {
+    //make an object of type CreateEmergencyRequest
+    //use createEmergency to make the emergency in DB
+    //redirect back to main page
+    let emergency: CreateEmergencyRequest = {
+      title: emergencyTitle,
+      overview: {
+        Importance: importance,
+        "Mechanism of Injury": mechanismOfInjury.split(","),
+        Diagnosis: diagnosis.split(","),
+        "Physical Exam": physicalExam.split(","),
+      },
+      treatment: {
+        "Acute Management": acuteManagement.split(","),
+        Dispo: dispo.split(","),
+        Considerations: considerations,
+      },
+      content: {},
+    };
+
+    createEmergency(emergency).then((result) => {
+      if (result.success) {
+        // clear the form
+        setEmergencyTitle("");
+        setImportance("");
+        setRiskFactors("");
+        setMechanismOfInjury("");
+        setDiagnosis("");
+        setPhysicalExam("");
+        setAcuteManagement("");
+        setDispo("");
+        setConsiderations("");
+        //redirect to homepage/main page
+        router.push("/");
+      } else {
+        // You should always clearly inform the user when something goes wrong.
+        // In this case, we're just doing an `alert()` for brevity, but you'd
+        // generally want to show some kind of error state or notification
+        // within your UI. If the problem is with the user's input, then use
+        // the error states of your smaller components (like the `TextField`s).
+        // If the problem is something we don't really control, such as network
+        // issues or an unexpected exception on the server side, then use a
+        // banner, modal, popup, or similar.
+
+        alert(result.error);
+        console.log(result);
+      }
+    });
+  };
 
   return (
-    <div style = {styles.page}>
-      <div style = {styles.container}>
-        <p style = {styles.header}>
-          Global Search &gt; Medical &gt; Add an injury
-        </p>
-        <p style = {styles.subtitle}>
-          Injury details
-        </p>
-        <p style = {styles.information}>
-          Name of injury*
-        </p>
-        <input
-        type="text"
-        placeholder="Enter a name"
-        style={styles.textbox}
+    <form style={styles.page}>
+      <div style={styles.container}>
+        <p style={styles.header}>Global Search &gt; Medical &gt; Add an injury</p>
+        <p style={styles.subtitle}>Injury details</p>
+        <InputBlock label="Name of injury*" value={emergencyTitle} onChange={setEmergencyTitle} />
+
+        <p style={styles.subheader}>Overview</p>
+
+        <InputBlock label="Importance" value={importance} onChange={setImportance} />
+
+        <InputBlock label="Risk factors" value={riskFactors} onChange={setRiskFactors} />
+
+        <InputBlock
+          label="Mechanism of Injury"
+          value={mechanismOfInjury}
+          onChange={setMechanismOfInjury}
         />
 
+        <InputBlock label="Diagnosis" value={diagnosis} onChange={setDiagnosis} />
 
-        <p style = {styles.subheader}>
-          Overview
-        </p>
+        <InputBlock label="Physical Exam" value={physicalExam} onChange={setPhysicalExam} />
 
-        <p style = {styles.information}>
-          Importance
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
+        <p style={styles.subheader}>How to Treat</p>
+
+        <InputBlock
+          label="Acute Management"
+          value={acuteManagement}
+          onChange={setAcuteManagement}
         />
 
-        <p style = {styles.information}>
-          Risk factors
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
-        />
+        <InputBlock label="Dispo" value={dispo} onChange={setDispo} />
 
-        <p style = {styles.information}>
-          Mechanism of Injury
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
-        />
-
-        <p style = {styles.information}>
-          Diagnosis
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
-        />
-
-        <p style = {styles.information}>
-          Physical Exam
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
-        />
-
-
-        <p style = {styles.subheader}>
-          How to treat
-        </p>
-
-        <p style = {styles.information}>
-          Acute Management
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
-        />
-
-        <p style = {styles.information}>
-          Dispo
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
-        />
-
-        <p style = {styles.information}>
-          Considerations
-        </p>
-        <input
-        type="text"
-        placeholder="Add details"
-        style={styles.textbox}
-        />
-
+        <InputBlock label="Considerations" value={considerations} onChange={setConsiderations} />
       </div>
 
       <div style={styles.buttonContainer}>
-        <button style={styles.cancelButton}>
-          Close
-        </button>
-        <button style={styles.publishButton}>
+        <button style={styles.cancelButton}>Close</button>
+        <button style={styles.publishButton} onClick={handlePublish}>
           Publish
         </button>
       </div>
-    </div>
-
-    
+    </form>
   );
 };
 
