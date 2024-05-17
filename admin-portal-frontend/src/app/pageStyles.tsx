a90b48222d7ba8d88bd2ed76ee5dd3a4d4d2a877
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-05-16 18:09:15 -0700

    Update pageStyles for explicit types

--- admin-portal-frontend/src/app/pageStyles.tsx
+++ admin-portal-frontend/src/app/pageStyles.tsx
@@ -1,22 +1,21 @@
-const styles = {
-    verticalNavBar: {
-      // position: 'absolute' as const,
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: '100%',
-      zIndex: 0
-    },
-    horizontalNavBar: {
-      height: '72px',
-      zIndex: 1,
-      // position: 'absolute' as const
-      position: 'fixed',
-    },
-    emergencyFlow: {
-      zIndex: 2,
-    },
-  };
-  
+import { CSSProperties } from "react";
+
+const styles: { [key: string]: CSSProperties } = {
+  verticalNavBar: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    zIndex: 0,
+  },
+  horizontalNavBar: {
+    height: "72px",
+    zIndex: 1,
+    position: "fixed",
+  },
+  emergencyFlow: {
+    zIndex: 2,
+  },
+};
+
 export default styles;
-  
