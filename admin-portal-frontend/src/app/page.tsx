26ac10a871b5cb15fdd131af3e1f6ce19cf2f592
Rohan Sachdeva <rosachdeva@ucsd.edu>
2024-05-21 09:20:51 -0700

    Fix page.tsx

--- admin-portal-frontend/src/app/page.tsx
+++ admin-portal-frontend/src/app/page.tsx
@@ -9,19 +9,12 @@ import Link from "next/link";
 const AnotherPage: React.FC = () => {
   return (
     <div>
-      {/* <div style={styles.verticalNavBar}>
+      <div style={styles.verticalNavBar}>
         <VerticalNavBar />
       </div>
       <div style={styles.horizontalNavBar}>
         <HorizontalNavBar />
-        <Link href="/emergencies">Go to emergencies</Link>
-        <Link href="/general-principles">Go to general principles</Link>
-        <Link href="/all-page">Go to pages</Link>
-      </div> */}
-      {/* <EmergencyFlow /> */}
-      <Link href="/emergencies">Go to emergencies</Link>
-      <Link href="/general-principles">Go to general principles</Link>
-      <Link href="/all-page">Go to pages</Link>
+      </div>
     </div>
   );
 };
