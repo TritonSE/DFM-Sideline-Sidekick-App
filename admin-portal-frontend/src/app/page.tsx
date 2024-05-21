0720aff5e05cdb99529bb82812108a65bdce252c
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-05-20 21:12:42 -0700

    Remove contents from page.tsx

--- admin-portal-frontend/src/app/page.tsx
+++ admin-portal-frontend/src/app/page.tsx
@@ -4,9 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-between p-24">
-      <Link href="/emergencies">Go to emergencies</Link>
-      <Link href="/general-principles">Go to general principles</Link>
-      <Link href="/all-page">Go to pages</Link>
     </main>
   );
 }
