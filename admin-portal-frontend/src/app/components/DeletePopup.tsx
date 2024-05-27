a3dbca891f3097a3a7fbd16e93dd61f3ed205b7f
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-05-27 12:12:46 -0700

    Add type to icon in delete popup

--- admin-portal-frontend/src/app/components/DeletePopup.tsx
+++ admin-portal-frontend/src/app/components/DeletePopup.tsx
@@ -2,6 +2,11 @@ import React from "react";
 
 import CloseIcon from "../icons/close.svg";
 
+type IconProps = {
+  'content-type': string,
+  src: string
+}
+
 type DeleteConfirmationPopupProps = {
   onDelete: () => void;
   onCancel: () => void;
@@ -15,7 +20,7 @@ const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
       <div className="bg-white w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 p-8 rounded-md shadow-md flex flex-col justify-center">
         <button className="flex justify-end mb-4" onClick={onCancel}>
-          <img src={CloseIcon?.src as string} alt="Close" className="w-4 h-4" />
+          <img src={(CloseIcon as IconProps).src} alt="Close" className="w-4 h-4" />
         </button>
         <div className="text-center">
           <h1 className="text-xl sm:text-2xl text-[#182B49] font-bold">
