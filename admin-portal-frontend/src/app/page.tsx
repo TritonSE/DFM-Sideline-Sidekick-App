584f9ab82eccec8ce490cf73eb854e908a89f8ff
LillianHo5 <holillian5@gmail.com>
2024-05-27 23:58:45 -0700

    Undo changes to page.tsx

--- admin-portal-frontend/src/app/page.tsx
+++ admin-portal-frontend/src/app/page.tsx
@@ -3,18 +3,16 @@ import React from "react";
 import HorizontalNavBar from "./components/HorizontalNavbar";
 import VerticalNavBar from "./components/VerticalNavBar";
 import styles from "./pageStyles";
-import AllPage from "./all-page/page";
 
 const AnotherPage: React.FC = () => {
   return (
     <div>
-      {/* <div style={styles.verticalNavBar}>
+      <div style={styles.verticalNavBar}>
         <VerticalNavBar />
       </div>
       <div style={styles.horizontalNavBar}>
         <HorizontalNavBar />
-      </div> */}
-      <AllPage />
+      </div>
     </div>
   );
 };
