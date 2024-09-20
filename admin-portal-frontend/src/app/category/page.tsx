49f72e93d9b996a770ff9b976490e265d388ca9c
anthony-pertsel <76824702+Anthonyp0329@users.noreply.github.com>
2024-09-20 12:07:06 -0700

    lint fixes

--- admin-portal-frontend/src/app/category/page.tsx
+++ admin-portal-frontend/src/app/category/page.tsx
@@ -7,7 +7,7 @@ import { Category, deletePage } from "../api/Categories";
 import PageContainer from "../components/PageContainer";
 import Toast from "../components/Toast";
 
-export default function CategoriesPage() {
+function CategoryInfo() {
   const searchParams = useSearchParams();
   const categoryString = searchParams.get("category");
   const [category, setCategory] = useState(
@@ -42,30 +42,32 @@ export default function CategoriesPage() {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex flex-col items-center justify-center min-h-screen w-screen overflow-auto p-20 gap-8 bg-[#E5EFF5]">
-        <div className="flex flex-row justify-between w-5/6">
-          <h1 className="text-start text-2xl font-bold">{category ? category.title : "ooga"}</h1>
-        </div>
-
-        <div className="flex flex-col h-max w-5/6 p-10 rounded-md bg-white">
-          <div className="flex flex-row items-center justify-between mb-10">
-            <h2 className="text-2xl">All Pages</h2>
-            <div className="flex flex-row flex-wrap justify-end gap-2">
-              <button className="px-4 py-2 rounded-md text-white bg-[#00629B]">Edit Order</button>
-              <button className="px-4 py-2 rounded-md text-white bg-[#00629B]">+ Add Page</button>
-            </div>
-          </div>
-          <PageContainer items={[category]} onDeletePage={onDeletePage}></PageContainer>
-          {showToast && (
-            <Toast
-              backgroundColor={"#000000"}
-              message={"Page deleted"}
-              onClose={handleCloseToast}
-            />
-          )}
-        </div>
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen overflow-auto p-20 gap-8 bg-[#E5EFF5]">
+      <div className="flex flex-row justify-between w-5/6">
+        <h1 className="text-start text-2xl font-bold">{category ? category.title : "ooga"}</h1>
       </div>
+
+      <div className="flex flex-col h-max w-5/6 p-10 rounded-md bg-white">
+        <div className="flex flex-row items-center justify-between mb-10">
+          <h2 className="text-2xl">All Pages</h2>
+          <div className="flex flex-row flex-wrap justify-end gap-2">
+            <button className="px-4 py-2 rounded-md text-white bg-[#00629B]">Edit Order</button>
+            <button className="px-4 py-2 rounded-md text-white bg-[#00629B]">+ Add Page</button>
+          </div>
+        </div>
+        <PageContainer items={[category]} onDeletePage={onDeletePage}></PageContainer>
+        {showToast && (
+          <Toast backgroundColor={"#000000"} message={"Page deleted"} onClose={handleCloseToast} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default function CategoriesPage() {
+  return (
+    <Suspense>
+      <CategoryInfo />
     </Suspense>
   );
 }
